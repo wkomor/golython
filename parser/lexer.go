2304af09506ea865230bc76d8acce388cb9efb4e
Vitold Komorovskiy <vkomorovski@dpkapp.ru>
2022-05-16 22:50:53 +0300

    add scaners

--- parser/lexer.go
+++ parser/lexer.go
@@ -79,3 +79,56 @@ func (s *Scanner) Scan() (tok Token, lit string) {
 
 	return ILLEGAL, string(ch)
 }
+
+// scanWhitespace consumes the current rune and all contiguous whitespace.
+func (s *Scanner) scanWhitespace() (tok Token, lit string) {
+	// Create a buffer and read the current character into it.
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	// Read every subsequent whitespace character into the buffer.
+	// Non-whitespace characters and EOF will cause the loop to exit.
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if !isWhitespace(ch) {
+			s.unread()
+			break
+		} else {
+			buf.WriteRune(ch)
+		}
+	}
+
+	return WS, buf.String()
+}
+
+// scanIdent consumes the current rune and all contiguous ident runes.
+func (s *Scanner) scanIdent() (tok Token, lit string) {
+	// Create a buffer and read the current character into it.
+	var buf bytes.Buffer
+	buf.WriteRune(s.read())
+
+	// Read every subsequent ident character into the buffer.
+	// Non-ident characters and EOF will cause the loop to exit.
+	for {
+		if ch := s.read(); ch == eof {
+			break
+		} else if !isLetter(ch) && !isDigit(ch) && ch != '_' {
+			s.unread()
+			break
+		} else {
+			_, _ = buf.WriteRune(ch)
+		}
+	}
+
+	// If the string matches a keyword then return that keyword.
+	switch strings.ToUpper(buf.String()) {
+	case "SELECT":
+		return SELECT, buf.String()
+	case "FROM":
+		return FROM, buf.String()
+	}
+
+	// Otherwise return as a regular identifier.
+	return IDENT, buf.String()
+}
